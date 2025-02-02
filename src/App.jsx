9efb69115e941b1a2dd3cc98e1153ca4a8cb95ea
TajwarSadi0151 <tajwaranjum015156552@gmail.com>
2025-02-02 15:57:52 +0600

    secoond commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1>Sadi</h1>
+     
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Root/Root.jsx
@@ -0,0 +1,13 @@
+import Footer from "../Footer/Footer";
+import Navbar from "../Navbar/Navbar";
+
+const Root = () => {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default Root;
